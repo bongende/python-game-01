608165911de718bd23fe83754508bf3264233feb
ChristoBongende <100066566+bongende@users.noreply.github.com>
2022-07-06 23:48:17 +0000

    i had function in those paddle and i create a pen

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-#paddle A
+# paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -21,7 +21,7 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-#paddle B
+# paddle B
 
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
@@ -31,39 +31,62 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-#Ball
+# Ball
 
 ball = turtle.Turtle()
 ball.speed(0)
-ball.shape("circle")
+ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
+ball.dx = 0.3
+ball.dy = 0.3
+
+# pen
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("player A: 0 player B: 0", align="center", font=("Courier", 24, "normal"))
 
 
-#Functions for paddles
+# Functions for paddles
 
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
+    if paddle_a.ycor() > 240 :
+        y = 240
     paddle_a.sety(y)
+
 
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
+    if paddle_a.ycor() < -240 :
+        y = -240
     paddle_a.sety(y)
-  
+
+
+
 def paddle_b_up():
-  y = paddle_b.ycor()
-  y += 20
-  paddle_b.sety(y)
+    y = paddle_b.ycor()
+    y += 20
+    if paddle_b.ycor() > 240 :
+        y = 240
+    paddle_b.sety(y)
+
+
 
 def paddle_b_down():
-  y = paddle_b.ycor()
-  y -= 20
-  paddle_b.sety(y)
+    y = paddle_b.ycor()
+    y -= 20
+    if paddle_b.ycor() < -240 :
+        y = -240
+    paddle_b.sety(y)
 
 
 # Keyboard
@@ -74,32 +97,35 @@ wn.onkeypress(paddle_a_down, "s")
 wn.onkeypress(paddle_b_up, "Up")
 wn.onkeypress(paddle_b_down, "Down")
 
-#Main game loop
+# Main game loop
 
 while True:
-  wn.update()
+    wn.update()
+    """"
+    ans = input("type \"yes\" if you want to continte")
+    if ans == "yes": """
 
-  #move thez ball
-  ball.setx(ball.xcor() + ball.dx)
-  ball.sety(ball.ycor() + ball.dy)
+    # move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
 
-  #border checking 
-  if ball.ycor() > 290 :
-    ball.sety(290)
-    ball.dy *= -1
-  elif ball.ycor() < -290 :
-    ball.sety(-290)
-    ball.dy *= -1
-  elif ball.xcor() > 390 :
-    ball.goto(0, 0)
-    ball.dx *= -1
-  elif ball.xcor() < -390 :
-    ball.goto(0, 0)
-    ball.dx *= -1
+    # border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+    elif ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+    elif ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+    elif ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
 
-  # paddle and ball collision
+    # paddle and ball collision
 
-   
-    
-  
-  
+    if ball.xcor() > 330 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40 ) :
+        ball.dx *= -1
+    if ball.xcor() < -330 and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40 ) :
+        ball.dx *= -1
